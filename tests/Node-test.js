00d4d6db207c9f6db818cbd3334c2dd1bf49f6b6
julie hawkins <julabi@gmail.com>
2017-10-24 22:15:38 -0600

    adds select method and index.js, cleans lint errors

--- tests/Node-test.js
+++ tests/Node-test.js
@@ -1,9 +1,6 @@
-// import { assert } from 'chai'
-// import Node from '../lib/Node'
-const chai = require('chai');
-const assert = chai.assert;
+import { assert } from 'chai'
+import Node from '../lib/Node'
 
-const Node = require('../lib/Node.js')
 let node;
 
 describe('Node', () => {
@@ -16,17 +13,14 @@ describe('Node', () => {
   });
 
   it('Should take in a letter and assign it to letter prop', () => {
-    // let node = new Node('p');
     assert.equal(node.letter, 'p');
   });
 
   it('Should have child prop and the value should be set to an object', () => {
-    // let node = new Node('p');
     assert.deepEqual(node.children, {});
   });
 
   it('Should have a prop isWord with a default setting of false', () => {
-    // let node = new Node('p');
     assert.equal(node.isWord, false);
   });
 });
