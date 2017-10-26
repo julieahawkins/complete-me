90dc066277bb254c33b508af52500e31dc0ecb4b
julie hawkins <julabi@gmail.com>
2017-10-25 21:34:17 -0600

    configures webpack, html

--- index.js
+++ index.js
@@ -5,3 +5,20 @@ module.exports = {
   Node,
   Trie
 }
+
+const wordTrie = new Trie();
+const input  = document.querySelector('input');
+
+wordTrie.populate(['blue', 'green', 'boy', 'gary', 'appeal', 'apple', 'appear']);
+
+input.addEventListener('keyup', inputSuggest);
+
+function inputSuggest() {
+  let suggestions = wordTrie.suggest(input.value);
+
+  suggestions.forEach((word) => {
+    const suggestList = document.querySelector('#suggestions');
+    suggestList.innerHTML += word
+  })
+};
+
