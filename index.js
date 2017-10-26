4edbd7831f99aa6c5e833d87ee47bc91abff3ec0
julie hawkins <julabi@gmail.com>
2017-10-26 09:52:53 -0600

    added dictionary to webpack

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const Node = require('./lib/Node.js');
 const Trie = require('./lib/Trie.js');
 
+// import fs from 'fs';
+const text = "./dictionary/words"
+// const dictionary = fs.readFileSync(text).toString().trim().split('\n')
+
+
 module.exports = {
   Node,
   Trie
 }
 
+$.get(text, function (data) {
+    let words = data;
+    let dictionary = words.toString().trim().split('\n');
+    
+    wordTrie.populate(dictionary);
+  });
+
 const wordTrie = new Trie();
 
-wordTrie.populate(['blue', 'banana', 'green', 'band', 'gary', 'appeal', 'apple', 'appear']);
+// wordTrie.populate(['blue', 'banana', 'green', 'band', 'gary', 'appeal', 'apple', 'appear']);
 
 $('input').on('keyup', inputSuggest);
 
